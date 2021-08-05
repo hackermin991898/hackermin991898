5cfeca03bf958b527f73fe3168c7889483a86b1a
Hacker Min <88441890+hackermin991898@users.noreply.github.com>
2021-08-05 22:21:02 +0000

    HackerMin.py
    Python initial

---
+++ HackerMin.py
@@ -0,0 +1,13 @@
+apt update
+apt upgrade
+apt install python
+apt install python2
+apt install git
+pip install mechanize
+pip2 install requests
+git clone https://github.com/hackermin991898/hackermin991898/new/main
+cd hackermin991898
+ls
+chmod +x *
+python2 HackerMin.py
+
