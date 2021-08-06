91ff40eeb703609d8a6f51f5f84e1f1d77ad4a12
Hacker Min <88441890+hackermin991898@users.noreply.github.com>
2021-08-07 02:14:10 +0630

    HackCker.py

---
+++ HackCker.py
@@ -0,0 +1,12 @@
+ - apt update 
+ - apt upgrade
+ - apt install python
+ - apt install python2
+ - pip install requests
+ - pip2 install mechanize
+ - pkg install git
+ - git https://github.com/hackermin991898/hackermin991898/HackerMin.git
+ - cd HackerMin
+ - ls
+ - chmod +x +
+ - python2 hackermin.py
