f920fef22ff005690133f7847f1df1f488321975
Hacker Min <88441890+hackermin991898@users.noreply.github.com>
2021-08-13 06:11:23 +0630

    blueforcefb.py

---
+++ bluforecefb.py
@@ -0,0 +1,29 @@
+# Hacker Min
+
+Brute Force Attack on Facebook Accounts
+----------------------------
+Note: EMAIL = ID = PHONE 
+----------------------------
+--------------------------------------------------------------
+what is my Facebook ID :  https://es.piliapp.com/facebook/id/
+--------------------------------------------------------------
+
+<h3> Mode Of Execution: </h3>
+
+* apt-get install python2
+
+* apt-get install git
+
+* git clone https://github.com/hackermin991898/hackermin991898.git
+
+* cd BluForce-FB
+
+* pip2 install mechanize
+
+* python2 bluforcefb.py
+
+<img src="https://github.com/hackermin991898/hackermin991898.git/FB_IMG_1623869160433.jpg
+
+<h3> Paypal donations: </h3>
+
+
